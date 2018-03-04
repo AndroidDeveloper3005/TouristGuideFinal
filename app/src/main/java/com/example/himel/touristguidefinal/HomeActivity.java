217b0b3a4c0e7538d2a889e4de41a741b401fc1f
AndroidDeveloper3005 <36477307+AndroidDeveloper3005@users.noreply.github.com>
2018-03-04 22:50:54 +0600

    Initial commit

---
+++ app/src/main/java/com/example/himel/touristguidefinal/HomeActivity.java
@@ -0,0 +1,69 @@
+package com.example.himel.touristguidefinal;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private ActionBar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+        if (!SharedPrefManager.getInstance(this).isLoggedIn()){
+            finish();
+            startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+
+        }
+
+
+        toolbar = getSupportActionBar();
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+       // toolbar.setTitle("Shop");
+        loadFragment(new HomeFragment());
+    }
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            android.support.v4.app.Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    toolbar.setTitle("Home");
+                    return true;
+                case R.id.navigation_trips:
+                    toolbar.setTitle("My Trips");
+                    return true;
+                case R.id.navigation_me:
+                    toolbar.setTitle("Me");
+                    return true;
+            }
+
+            return false;
+
+        }
+    };
+
+    private void loadFragment(android.support.v4.app.Fragment fragment) {
+        // load fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+}
